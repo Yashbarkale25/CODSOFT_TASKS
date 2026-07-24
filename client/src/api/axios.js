0e1b32d96f8fab7a89b2be5397b87e44f2661d84
Yash Barkale <yashbarkale8@gmail.com>
2026-07-24 21:40:26 +0530

    Fix API base URL

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://job-board-backend-fuig.onrender.com",
+  baseURL: "https://job-board-backend-fuig.onrender.com/api",
 });
 
 API.interceptors.request.use((config) => {
