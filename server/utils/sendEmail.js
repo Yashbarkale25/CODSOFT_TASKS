e1e10285791e4a45f897b0f74b6d1ea909a7e80c
Yash Barkale <yashbarkale8@gmail.com>
2026-07-30 20:46:03 +0530

    Production API URL

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -12,19 +12,32 @@ const transporter = nodemailer.createTransport({
 
 transporter.verify((err) => {
   if (err) {
-    console.log("SMTP Error", err);
+    console.log("SMTP Error:", err);
   } else {
     console.log("✅ Brevo SMTP Connected");
   }
 });
 
 const sendEmail = async (to, subject, text) => {
-  return transporter.sendMail({
-    from: `"Job Portal" <${process.env.EMAIL_USER}>`,
-    to,
-    subject,
-    text,
-  });
+  try {
+    const info = await transporter.sendMail({
+      from: `"Job Portal" <${process.env.SENDER_EMAIL}>`,
+      to,
+      subject,
+      text,
+    });
+
+    console.log("✅ Email Sent");
+    console.log("Message ID:", info.messageId);
+    console.log("Accepted:", info.accepted);
+    console.log("Rejected:", info.rejected);
+    console.log("Response:", info.response);
+
+    return info;
+  } catch (err) {
+    console.log("❌ Send Mail Error:", err);
+    throw err;
+  }
 };
 
 module.exports = sendEmail;
