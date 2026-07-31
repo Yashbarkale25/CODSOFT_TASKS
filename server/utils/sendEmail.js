6d4f8359a31540b9dc716b78461962c0055de995
Yash Barkale <yashbarkale8@gmail.com>
2026-07-31 23:26:42 +0530

    Use Brevo Email API

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -1,42 +1,45 @@
-const nodemailer = require("nodemailer");
-
-const transporter = nodemailer.createTransport({
-  host: "smtp-relay.brevo.com",
-  port: 587,
-  secure: false,
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
-  },
-});
-
-transporter.verify((err) => {
-  if (err) {
-    console.log("SMTP Error:", err);
-  } else {
-    console.log("✅ Brevo SMTP Connected");
-  }
-});
+const axios = require("axios");
 
 const sendEmail = async (to, subject, text) => {
   try {
-    const info = await transporter.sendMail({
-      from: `"Job Portal" <${process.env.SENDER_EMAIL}>`,
-      to,
-      subject,
-      text,
-    });
+    const response = await axios.post(
+      "https://api.brevo.com/v3/smtp/email",
+      {
+        sender: {
+          name: "Job Portal",
+          email: process.env.SENDER_EMAIL,
+        },
+        to: [
+          {
+            email: to,
+          },
+        ],
+        subject: subject,
+        textContent: text,
+      },
+      {
+        headers: {
+          accept: "application/json",
+          "api-key": process.env.BREVO_API_KEY,
+          "content-type": "application/json",
+        },
+      }
+    );
 
-    console.log("✅ Email Sent");
-    console.log("Message ID:", info.messageId);
-    console.log("Accepted:", info.accepted);
-    console.log("Rejected:", info.rejected);
-    console.log("Response:", info.response);
+    console.log("✅ Email Sent Successfully");
+    console.log(response.data);
 
-    return info;
-  } catch (err) {
-    console.log("❌ Send Mail Error:", err);
-    throw err;
+    return response.data;
+  } catch (error) {
+    console.log("❌ Brevo API Error");
+
+    if (error.response) {
+      console.log(error.response.data);
+    } else {
+      console.log(error.message);
+    }
+
+    throw error;
   }
 };
 
